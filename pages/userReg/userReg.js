3ccbaf219bf5755dea2211e89bb4378d1b9e887d
XiaoShi <337265223@qq.com>
2025-06-11 15:58:42 +0800

    Update userReg.js

--- pages/userReg/userReg.js
+++ pages/userReg/userReg.js
@@ -5,14 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    avatarUrl: 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+    avatarUrl: 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0',
+    nickName: '',
+    phone: ''
   },
   onInputChange(e) {
     const nickName = e.detail.value
-    const { avatarUrl } = this.data.userInfo
     this.setData({
-      "userInfo.nickName": nickName,
-      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+      "nickName": nickName,
     })
   },
 })
