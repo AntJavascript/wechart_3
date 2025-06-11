1fd85837566d782639044342008fdc651efd2bf5
XiaoShi <337265223@qq.com>
2025-06-11 16:28:09 +0800

    Update userReg.js

--- pages/userReg/userReg.js
+++ pages/userReg/userReg.js
@@ -20,5 +20,22 @@ Page({
     this.setData({
       avatarUrl,
     })
+  },
+  login(){
+    wx.login({
+  success (res) {
+    if (res.code) {
+      //发起网络请求
+      wx.request({
+        url: 'https://example.com/onLogin',
+        data: {
+          code: res.code
+        }
+      })
+    } else {
+      console.log('登录失败！' + res.errMsg)
+    }
+  }
+})
   }
 })
