aaae36df04938278cacd5b5601602b8eb257c5d5
XiaoShi <337265223@qq.com>
2025-06-11 15:54:29 +0800

    Update userReg.js

--- pages/userReg/userReg.js
+++ pages/userReg/userReg.js
@@ -5,6 +5,6 @@ Page({
    * 页面的初始数据
    */
   data: {
-    avatar: 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+    avatarUrl: 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
   },
 })
