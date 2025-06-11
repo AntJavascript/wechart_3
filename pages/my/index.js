10956fbbe18ef677b689e7fc2f41f11823c89127
XiaoShi <337265223@qq.com>
2025-06-11 14:48:42 +0800

    Update index.js

--- pages/my/index.js
+++ pages/my/index.js
@@ -6,7 +6,7 @@ Page({
    */
   data: {
     isLogin: false,
-    defaultAvatarUrl: "https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0"
+    defaultAvatarUrl: 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
   },
 
   /**
