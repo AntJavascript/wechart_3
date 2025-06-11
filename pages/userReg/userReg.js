9407efc3b03363fa696be046c92ff3eb9d3ed902
XiaoShi <337265223@qq.com>
2025-06-11 15:57:20 +0800

    Update userReg.js

--- pages/userReg/userReg.js
+++ pages/userReg/userReg.js
@@ -7,4 +7,12 @@ Page({
   data: {
     avatarUrl: 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
   },
+  onInputChange(e) {
+    const nickName = e.detail.value
+    const { avatarUrl } = this.data.userInfo
+    this.setData({
+      "userInfo.nickName": nickName,
+      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+    })
+  },
 })
