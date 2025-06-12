f64d87b7e14b25389e520bac04a940849dbad512
XiaoShi <337265223@qq.com>
2025-06-12 14:28:46 +0800

    Update regTeacher.js

---
+++ pages/regTeacher/regTeacher.js
@@ -0,0 +1,21 @@
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name: "", // 姓名
+    phone: "", // 联系电话
+    price: "", // 价格
+    discipline: "", // 教学学科
+    teachingYears: "", // 教龄
+    personalPhotos: [], // 个人照片
+    education: "", // 学历
+    school: "", // 毕业院校
+    teachingMethod: "", // 授课方式
+    personaldesc: "", // 个人描述
+    IDCard: [], // 身份证
+    academicCertificates: [], // 学历证书
+    vocationalCertificates: [], // 职业证书
+    HonorCertificates: [], // 荣誉证书
+  },
+})
