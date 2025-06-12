9f95e92895b1b1fefea8049d7a679aed309ffffa
XiaoShi <337265223@qq.com>
2025-06-12 15:46:44 +0800

    Update regTeacher.js

--- pages/regTeacher/regTeacher.js
+++ pages/regTeacher/regTeacher.js
@@ -7,10 +7,13 @@ Page({
     phone: "", // 联系电话
     price: "", // 价格
     discipline: "", // 教学学科
+    disciplineArray: ['管理学', '医学', '农学', '工学'], // 教学学科选项
     teachingYears: "", // 教龄
     education: "", // 学历
+    educationArray: ["专科","本科","硕士研究生","博士研究生"], // 学历选项
     school: "", // 毕业院校
     teachingMethod: "", // 授课方式
+    teachingMethodArray: ['学生上门', '网络授课', '上门授课'], // 授课方式选项
     personaldesc: "", // 个人描述
     personalPhotos: [], // 个人照片
     IDCard: [], // 身份证
@@ -18,7 +21,13 @@ Page({
     vocationalCertificates: [], // 职业证书
     HonorCertificates: [], // 荣誉证书
   },
-  bindDisciplineChange(){
+  bindDisciplineChange(e){
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+  },
+  bindEducationChange(){
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+  },
+  bindTeachingMethodChange(){
     console.log('picker发送选择改变，携带值为', e.detail.value)
   }
 })
