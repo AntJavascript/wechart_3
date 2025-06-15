de4b31ad4262d8aedbfd55b2025d34e1ae0ddf90
XiaoShi <337265223@qq.com>
2025-06-15 18:15:51 +0800

    提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,4 @@
 // index.js
-const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
-
 Page({
   data: {
     islogin: false, // 是否已经登录
@@ -16,45 +14,24 @@ Page({
       url: '/pages/userReg/userReg'
     })
   },
+  getPhoneNumber (e) {
+    console.log(e.detail.code)  // 动态令牌
+    console.log(e.detail.errMsg) // 回调信息（成功失败都会返回）
+    console.log(e.detail.errno)  // 错误码（失败时返回）
+  },
   onLoad() {
-    wx.checkSession({
-      success: () => {
-        this.setData({isLogin: true})
-        //session_key 未过期，并且在本生命周期一直有效
-      },
-      fail: () => {
-        // session_key 已经失效，需要重新执行登录流程
-        this.setData({isLogin: false})
-        wx.login({
-          success (res) {
-            if (res.code) {
-              //发起网络请求
-              wx.request({
-                url: 'https://example.com/onLogin',
-                data: {
-                  code: res.code
-                }
-              })
-            } else {
-              console.log('登录失败！' + res.errMsg)
-            }
+    wx.login({
+        success (res) {
+          if (res.code) {
+            // 发起网络请求
+            wx.request({
+              url: `http://192.168.1.104:8080/login/checkSessionKey/${res.code}`,
+            })
+          } else {
+            console.log('登录失败！' + res.errMsg)
           }
-        }) //重新登录
-      }
-    })
-    // wx.request({
-    //     url: 'http://192.168.1.104:8080/login/saveUserInfo', //仅为示例，并非真实的接口地址
-    //    method: 'POST',
-    //    data:{
-    //        id: 11
-    //    },
-    //     header: {
-    //       'content-type': 'application/json' // 默认值
-    //     },
-    //     success (res) {
-    //       console.log(res.data)
-    //     }
-    //   })
+        }
+      })
   },
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
