36059f8acfa888491b3a5da37a64eb40ce5856f4
XiaoShi <337265223@qq.com>
2025-06-16 09:30:08 +0800

    Update index.js

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,11 +14,6 @@ Page({
       url: '/pages/userReg/userReg'
     })
   },
-  getPhoneNumber (e) {
-    console.log(e.detail.code)  // 动态令牌
-    console.log(e.detail.errMsg) // 回调信息（成功失败都会返回）
-    console.log(e.detail.errno)  // 错误码（失败时返回）
-  },
   onLoad() {
     wx.login({
         success (res) {
