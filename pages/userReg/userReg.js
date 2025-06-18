13943873458819ca0fccc9201db3244be437ec74
XiaoShi <337265223@qq.com>
2025-06-18 14:35:08 +0800

    Update userReg.js

--- pages/userReg/userReg.js
+++ pages/userReg/userReg.js
@@ -5,21 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-    avatarUrl: 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0',
-    nickName: '',
-    phone: ''
+    name: '',
+    phone: '',
+    school: '',
+    address: ''
   },
-  onInputChange(e) {
-    const nickName = e.detail.value
-    this.setData({
-      nickName,
-    })
-  },
-  onChooseAvatar(e) {
-    const { avatarUrl } = e.detail 
-    this.setData({
-      avatarUrl,
-    })
+  toSelectMap(e) {
+   wx.chooseLocation({
+     success(res){
+       console.log(res)
+     }
+   })
   },
   login(){
     wx.login({
